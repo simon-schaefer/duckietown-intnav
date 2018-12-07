6c7dd2f25cda51538a0ca90e9ed35546a17ae556
SeleSchaefer <sischaef@student.ethz.ch>
2018-12-07 14:26:41 +0100

    docker executing file

---
+++ docker/run_docker.bash
@@ -0,0 +1,9 @@
+# /bin/bash
+
+DUCKIEBOT="sebot"
+DOCKER_NAME="seleschaefer"
+PACKAGE="intnav"
+TAG="0.1"
+
+docker -H $DUCKIEBOT.local run -dit --name intnav -v /data:/data $DOCKER_NAME/$PACKAGE:$TAG
+docker -H $DUCKIEBOT.local exec -it intnav /bin/bash
