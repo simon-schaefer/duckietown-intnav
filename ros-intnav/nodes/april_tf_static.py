cd6891568ef0e541b3804ce3a6bdabfcb5ce964e
SeleSchaefer <sischaef@student.ethz.ch>
2018-12-14 02:40:42 +0100

    vehicle and cam frame for each tag

--- ros-intnav/nodes/april_tf_static.py
+++ ros-intnav/nodes/april_tf_static.py
@@ -10,12 +10,25 @@ import tf
 
 def publish_trafo(event):
     br = tf.TransformBroadcaster()
-    world_frame = rospy.get_param("tf_april_static/world_frame")
+    duckiebot = rospy.get_param("tf_april_static/duckiebot")
+    cam_frame = rospy.get_param("tf_april_static/cam_frame")
+    vehicle_frame = rospy.get_param("tf_april_static/vehicle_frame")
+    world_frame = rospy.get_param("tf_april_static/world_frame")    
+    prefix = duckiebot + "/params/"
+    trans = (rospy.get_param(prefix + "cam_x"), 
+             rospy.get_param(prefix + "cam_y"), 
+             rospy.get_param(prefix + "cam_z"))
+    quat = (rospy.get_param(prefix + "cam_qx"), 
+            rospy.get_param(prefix + "cam_qy"), 
+            rospy.get_param(prefix + "cam_qz"), 
+            rospy.get_param(prefix + "cam_qw"))
     for tag in rospy.get_param("apriltags/standalone_tag_poses"):
         br.sendTransform((tag['x'], tag['y'], tag['z']),
             (tag['qx'], tag['qy'], tag['qz'], tag['qw']), 
             rospy.Time.now(), 
             "tag" + str(tag['id']), world_frame)
+        br.sendTransform(trans, quat, rospy.Time.now(), 
+            vehicle_frame+str(tag['id']), cam_frame+str(tag['id']))
 
 if __name__ == '__main__':
     rospy.init_node('tf_april_static')
